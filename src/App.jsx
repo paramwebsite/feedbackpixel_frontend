fe2b7ba3830d19572407e3017d0376791664fc7f
software-paraminnovation <software@paraminnovation.org>
2025-10-15 16:47:01 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Page1 from "./pages/Page1";
 import Page2 from "./pages/Page2";
-import Page3 from "./pages/page3";
-import Page4 from "./pages/page4";
-import Page5 from "./pages/page5";
-import Page6 from "./pages/page6";
+import Page3 from "./pages/Page3";
+import Page4 from "./pages/Page4";
+import Page5 from "./pages/Page5";
+import Page6 from "./pages/Page6";
 
 //
 
